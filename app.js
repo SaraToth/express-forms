89ff97a22669dd1961eca28c931164049f2329a0
SaraToth <saratoth101@gmail.com>
2025-06-18 19:40:11 +0900

    Feat: Create a mock database

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const { body, validationResult } = require("express-validator");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended:true }));
+
 app.get("/", (req, res) => {
     res.render("index");
 });
