da5d9595f88a46f6f066d3d4021e288bfcee18c9
SaraToth <saratoth101@gmail.com>
2025-06-18 19:58:55 +0900

    Feat: Update index view to list users

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,9 @@
 const { Router } = require("express");
 const indexRouter = Router();
-const usersStorage = require("../storages/usersStorage");
 const { getCreateUser, postCreateUser, getIndex } = require("../controllers/indexController");
 
 indexRouter.get("/create", getCreateUser);
-
 indexRouter.post("/create", postCreateUser);
-
 indexRouter.get("/", getIndex);
 
 module.exports = indexRouter;
