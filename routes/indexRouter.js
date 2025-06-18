e6b534dcb84744ea48c36ff58840841466cb8a69
SaraToth <saratoth101@gmail.com>
2025-06-18 20:57:09 +0900

    Feat: Create route to post updated user

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,7 +5,7 @@ const { getCreateUser, postCreateUser, getIndex, getUpdateUser, postUpdateUser }
 indexRouter.get("/create", getCreateUser);
 indexRouter.post("/create", postCreateUser);
 indexRouter.get("/:id/update", getUpdateUser);
-// indexRouter.post("/:id/update", postUpdateUser)
+indexRouter.post("/:id/update", postUpdateUser)
 indexRouter.get("/", getIndex);
 
 module.exports = indexRouter;
